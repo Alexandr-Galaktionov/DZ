25a60338e444b2580f78ba795d2c23b2672a1853
Aleksanr_Galaktionov <alexanr.galaktionov@yanex.ru>
2022-02-23 19:08:11 +0300

    программаа

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿string[] array = { "a","boby","cccp","mir","nikto","z", "5"};
+ 
+    var result = new string[array.Length];
+    var realSize = 0;
+    foreach (var value in array)
+        {
+        if (value.Length <= 3)
+        {
+        result[realSize] = value;
+        realSize++;
+     }
+    }
+ 
+    Console.WriteLine(string.Join(" ", result, 0, realSize));
